ebd5c90fd3bf476e660afe7edf524bc73de5ac68
ivan4000 <sxln1@mail.ru>
2018-12-04 22:27:19 +0300

    JL322 assertions

---
+++ src/com/Main.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+      /*  int x = 5;
+        switch (x) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                assert false;
+        }
+        if (true) {
+        } else if (false) {
+        } else {
+            assert false;
+        }
+        */
+        assert args.length > 0 :  new IOException("fail");
+
+    }
+   // static int getI(){return 5;}
+}
+
+
+
+
+
+
+
+
+
---
+++ src/com/Main2.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int x=33;
+       assert x<20:"Fail";
+        System.out.println("mission complete");
+    }
+}
